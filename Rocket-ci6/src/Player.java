af1c5fd7d2f71202695fd72180fd44473d46dd8e
Aboco <anhhvu89@gmail.com>
2018-06-03 18:05:21 +0700

    create PlayerMove class

--- Rocket-ci6/src/Player.java
+++ Rocket-ci6/src/Player.java
@@ -9,48 +9,32 @@ import java.util.stream.Collectors;
 public class Player {
 
     public Vector2D position;
-    public Vector2D velocity;
-    private Random random;
-    public double angle = 0.0;
-    private PolygonRenderer renderer;
+    public PlayerMove playerMove;
+    private Renderer renderer;
     private PlayerShoot playerShoot;
 
 
     public Player() {
         this.position = new Vector2D();
-        this.velocity = new Vector2D();
-        this.random = new Random();
         this.renderer = new PolygonRenderer(
                 Color.RED,
                 new Vector2D(),
                 new Vector2D(0, 16),
                 new Vector2D(20, 8)
         );
+        this.playerMove = new PlayerMove();
         this.playerShoot = new PlayerShoot();
     }
 
     public void run() {
-        this.position.addUp(this.velocity);
-        this.renderer.angle = this.angle;
-        this.backToScreen();
+        this.playerMove.run(this);
+
         this.playerShoot.run(this);
         this.playerShoot.bulletsPlayer.forEach(bulletPlayer -> bulletPlayer.run());
+        ((PolygonRenderer)(this.renderer)).angle = this.playerMove.angle;
     }
 
-    private void backToScreen() {
-        if (this.position.x > 1024) {
-            this.position.set(0, this.random.nextInt(600));
-        }
-        if (this.position.x < 0) {
-            this.position.set(1024, this.random.nextInt(600));
-        }
-        if (this.position.y > 600) {
-            this.position.set(this.random.nextInt(1024), 0);
-        }
-        if (this.position.y < 0) {
-            this.position.set(this.random.nextInt(1024), 600);
-        }
-    }
+
 
 
     public void render(Graphics graphics) {
