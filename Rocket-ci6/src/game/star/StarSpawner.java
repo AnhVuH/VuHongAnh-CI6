18fc6c04a41763cdb1a2fba19f706cc633e47147
Aboco <anhhvu89@gmail.com>
2018-06-17 00:12:35 +0700

    test actions

--- Rocket-ci6/src/game/star/StarSpawner.java
+++ Rocket-ci6/src/game/star/StarSpawner.java
@@ -18,20 +18,55 @@ public class StarSpawner extends GameObject {
 
 
     public void createAction(){
-        Action waitAction = new WaitAction(30);
-        Action createAction = new ActionAdapter() {
-            @Override
-            public boolean run(GameObject owner) {
-                Star star = GameObjectManager.instance.recycle(Star.class);star.position.set(1024, random.nextInt(600));
-                star.velocity.set(-(random.nextInt(2)+1),0);
-                return true;
-            }
-        };
+//        Action waitAction = new WaitAction(30);
+//        Action createAction = new ActionAdapter() {
+//            @Override
+//            public boolean run(GameObject owner) {
+//                Star star = GameObjectManager.instance.recycle(Star.class);star.position.set(1024, random.nextInt(600));
+//                star.velocity.set(-(random.nextInt(2)+1),0);
+//                return true;
+//            }
+//        };
+//
+//        Action sequenceAction = new SequenceAction(waitAction,createAction);
+//        Action repeatAction = new RepeatActionForever(sequenceAction);
+//
+//        this.addAction(repeatAction);
 
-        Action sequenceAction = new SequenceAction(waitAction,createAction);
-        Action repeatAction = new RepeatActionForever(sequenceAction);
+//        this.addAction(
+//                new RepeatActionForever(
+//                        new SequenceAction(
+//                            new WaitAction(30),
+//                            new ActionAdapter() {
+//                                @Override
+//                                public boolean run(GameObject owner) {
+//                                    Star star = GameObjectManager.instance.recycle(Star.class);star.position.set(1024, random.nextInt(600));
+//                                    star.velocity.set(-(random.nextInt(2)+1),0);
+//                                    return true;
+//                                }
+//                            }
+//
+//                        )
+//                )
+//        );
 
-        this.addAction(repeatAction);
+        this.addAction(
+                new LimitAction(
+                        new SequenceAction(
+                            new WaitAction(30),
+                            new ActionAdapter() {
+                                @Override
+                                public boolean run(GameObject owner) {
+                                    Star star = GameObjectManager.instance.recycle(Star.class);star.position.set(1024, random.nextInt(600));
+                                    star.velocity.set(-(random.nextInt(2)+1),0);
+                                    return true;
+                                }
+                            }
+
+                        )
+                ,10)
+        );
+
     }
 
     @Override
